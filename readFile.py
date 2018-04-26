25e5fbea89aee6471eba473e9249dd50f4b1f553
Xinhe Chen <xinhe.c@gmail.com>
2018-04-26 13:31:39 -0500

    working csv reader

--- readFile.py
+++ readFile.py
@@ -1,4 +1,7 @@
 import numpy as np
 import pandas as pd
 
-df = pd.read_csv('goodbooks-10k-master/ratings.csv', sep=',')
+data = pd.read_csv('goodbooks-10k-master/ratings.csv', sep=',')
+users = data.user_id.unique().shape[0]
+items = data.book_id.unique().shape[0]
+print ('Number of users = ' + str(users) + ' | Number of movies = ' + str(items))
